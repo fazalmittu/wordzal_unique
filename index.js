2608e91992a653f33c57df415a499174c82a8f64
Fazal Mittu <fazalmittu1800@gmail.com>
2022-04-07 21:19:17 -0700

    unlimited

--- index.js
+++ index.js
@@ -163,6 +163,33 @@ function processKey() {
 
 function initialize() {
 
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btnModal = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btnModal.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
     //creates initial "board" for tiles
     //separate id created for each tile based on indices in array (5x6)
 
@@ -400,7 +427,7 @@ function update() {
 
 function copyResult() {
 
-    let beginning = "WORDZAL_V2 " + row + "/" + 8 + "\n" + "\n";
+    let beginning = "WORDZAL UNLIMITED " + row + "/" + 8 + "\n" + "\n";
 
     navigator.clipboard.writeText(beginning + outputStr);
 
